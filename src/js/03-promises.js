dcd58e0bc17e3b7614378be306cd440ce8af5115
Akim Grakovskiy <aakim5586@gmail.com>
2021-12-09 22:44:07 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
+}
+
+console.log(refs);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
